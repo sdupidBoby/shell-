226155eec7358abf45bd9f3571a5dbb5c39a57c8
yuanshuang <1006228982@qq.com>
2018-07-09 16:23:39 +0800

    + Automator，workflow

--- ABuild.sh
+++ ABuild.sh
@@ -1,9 +1,7 @@
-# 打包Release or Debug
-BuildConfiguration=Release
-# 项目scheme名
-SCHEMENAME=demo     
-# 项目路径
-SOURCEPATH=/Users/admin/Desktop/demo   
+BuildConfiguration=Release # 打包Release or Debug
+SCHEMENAME=demo      # 项目scheme名
+SOURCEPATH=$PWD    # 项目路径
+
 
 DATE=`date +%Y%m%d_%H%m`
 # 打包目标路径
@@ -20,7 +18,7 @@ if [[ $? != 0 ]]; then
 fi
 
 
-# build
+# archive
 xcodebuild archive \
 -workspace $SOURCEPATH/$SCHEMENAME.xcworkspace \
 -scheme ${SCHEMENAME} \
@@ -42,11 +40,35 @@ xcodebuild -exportArchive \
 -exportOptionsPlist ${SOURCEPATH}/buildScript/AppStoreExportOptions.plist
 
 if [ -e ${IPAPATH}/${SCHEMENAME} ]; then
+    osascript -e 'display notification "打包成功！" with title "${IPAPATH}/${SCHEMENAME}"'
     echo "Archive OK"
     open ${IPAPATH}/${SCHEMENAME}
 else
     echo "Archive fail"
 fi
+
+###################################
+#发布到iTunesConnect
+###################################
+altoolPath="/Applications/Xcode.app/Contents/Applications/Application Loader.app/Contents/Frameworks/ITunesSoftwareService.framework/Versions/A/Support/altool"
+appleid=xxx@.163.com
+applepassword=xxx
+
+#validate
+"$altoolPath" --validate-app \
+-f "${IPAPATH}/${SCHEMENAME}/${IPANAME}"\
+-u "$appleid"\
+-p "$applepassword"\
+-t ios \
+osascript -e 'display notification "Release To AppStore" with title "Validate Complete!"'
+
+#upload
+"$altoolPath" --upload-app \
+-f "${IPAPATH}/${SCHEMENAME}/${IPANAME}" \
+-u "$appleid" -p "$applepassword" \
+-t ios \
+--output-format xml
+osascript -e 'display notification "Release To AppStore" with title "Upload Complete!"'
 
 
 
